5966e467c7a5d7e309f755658f06e94fd08f6d40
Shreshth Tuli <shreshthtuli@gmail.com>
2021-04-30 13:03:29 +0100

    Update function.py

--- function.py
+++ function.py
@@ -6,4 +6,4 @@ def function(input_file_path, output_file_path):
     with Image.open(input_file_path) as image:
         ratio = max(image.size) / float(args)
         image.thumbnail(tuple(int(x / ratio) for x in image.size))
-        image.save(resized_img_path)
+        image.save(output_file_path)
