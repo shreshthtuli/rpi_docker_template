4499eab34a08794e87522496edc385ed2895efff
Shreshth Tuli <shreshthtuli@gmail.com>
2021-04-30 12:59:30 +0100

    added template files

---
+++ function.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+THUMBNAIL_SIZES_PX = [200]
+
+def function(input_file_path, output_file_path):
+    with Image.open(input_file_path) as image:
+        ratio = max(image.size) / float(args)
+        image.thumbnail(tuple(int(x / ratio) for x in image.size))
+        image.save(resized_img_path)
